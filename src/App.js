56066a114970d3b6d89843ada88bc352242bc752
biskoi <biskoi@daydream.cafe>
2020-02-20 14:25:42 -0700

    Added a bunch of styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,51 @@ import React, {useState, useEffect} from "react";
 import "./App.css";
 import PhotoContainer from './PhotoContainer';
 import Axios from 'axios';
+import styled from 'styled-components';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Card, 
+  Jumbotron as ReactJumbotron,
+  Button as ReactButton
+} from 'reactstrap';
 // import dummyData from './dummydata';
+
+const PhotoCard = styled(Card)`
+  padding-bottom: 5%;
+  background-color: black;
+  color: white;
+`;
+
+const Button = styled(ReactButton)`
+margin: 2%;
+`;
+
+const Jumbotron = styled(ReactJumbotron)`
+background: black;
+color: white;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+padding: 0;
+margin: 2% 2%;
+border-bottom: solid 1px grey;
+`;
+
+const NavImg = styled.img`
+width: 3.5rem;
+margin: 0;
+`;
 
 function App() {
 
@@ -10,6 +54,8 @@ function App() {
   const today = date.getDate();
   const [day, setDay] = useState(today);
   const [item, setItem] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
 
   useEffect(() => {
       Axios.get(`https://api.nasa.gov/planetary/apod?api_key=tPqDzEjAJP8NZZruR315gRAp8E4u1Y5EjirNk2Hb&date=2020-02-${day}`)
@@ -36,16 +82,52 @@ function App() {
   return (
     <div className="App">
 
-      <h1>Nasa Photo of the Day</h1>
+        <Navbar color="dark" dark expand="md">
+          <NavbarBrand href="https://www.nasa.gov/"><NavImg src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/1280px-NASA_logo.svg.png'/></NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink href="https://biskoi.github.io/site">My Website</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://www.nasa.gov/">NASA Actual</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
 
-      <div className = 'PhotoContainer'>
-        
+        <Jumbotron>
+        <h1 className="display-3">Picture of the Day</h1>
+        <p className="lead">Each day, a new image will be shown here. You can also press the buttons below the image to look at past images.</p>
+      </Jumbotron>
+
+      <div className = 'PhotoContainer' id = 'PhotoContainer'>
+
+        <PhotoCard>
         <PhotoContainer data = {item}/>
-
         <div className = 'buttons'>
-          <button onClick = {() => photoHandler(0)}>Previous Photo</button>
-          <button onClick = {() => photoHandler(1)}>Next Photo</button>
+          <Button color = 'secondary' onClick = {() => photoHandler(0)}>Previous Photo</Button>
+          <Button color = 'secondary' onClick = {() => photoHandler(1)}>Next Photo</Button>
         </div>
+        </PhotoCard>
 
       </div>
 
--- src/PhotoContainer.js
+++ src/PhotoContainer.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import './PhotoContainer.css';
+import {CardImg as ReactCard, CardText as ReactCardText, CardBody, CardTitle as ReactTitle, CardSubtitle,} from 'reactstrap';
+import styled from 'styled-components';
+
+const CardImg = styled(ReactCard)`
+width: 50%;
+margin: 3%;
+border: solid white 1px;
+`;
+
+const CardTitle = styled(ReactTitle)`
+margin-top: 1%;
+`;
+
+const CardText = styled(ReactCardText)`
+margin: 0 10%;
+`;
 
 function PhotoContainer(props) {
 
     return (
 
         <div className = 'photoContainer'>
-            <h3>{props.data.title}, {props.data.date}</h3>
-            <img src = {props.data.url} alt = 'A certain celestial body'/>
-            <p>{props.data.explanation}</p>
+            <CardTitle tag = 'h2'>{props.data.title}, {props.data.date}</CardTitle>
+            <CardImg src = {props.data.url} alt = 'A certain celestial body'/>
+            <CardText>{props.data.explanation}</CardText>
         </div>
 
     )
